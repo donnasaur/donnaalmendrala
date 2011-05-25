cb03e85086e657e9df2f1f95e9e34382d374200f
Donna Almendrala <donna.almendrala@gmail.com>
2011-05-24 21:40:20 -0700

    updated site with links, store, and contact info

--- index.php
+++ index.php
@@ -1,32 +1,17 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
-
-<head profile="http://www.w3.org/2005/10/profile">
-<!--
+<?php
+/*
 All rights reserved for pages and content related to this site.
 
 All code licensed under the Apache 2.0 (see APACHE-LICENSE) and 
 the GNU GPL v3 licenses (see GPL-LICENSE).
 
 Copyright 2011 Donna Almendrala (see AUTHORS)
--->
-	<link rel="icon" type="image/png" href="images/favicon.png">
-	<title>Donna Almendrala</title>
-	<meta http-equiv="content-type" 
-		content="text/html;charset=utf-8" />
-	<link href="stylesheets/reset.css" media="all" rel="stylesheet" type="text/css" />
-	<link href="stylesheets/fonts.css" media="all" rel="stylesheet" type="text/css" />
-	<link href="stylesheets/public.css" media="all" rel="stylesheet" type="text/css" />
-</head>
+*/ ?>
 
-<body>
-	<div id="header">	
-    <h1>Website coming soon…</h1> 
-    <p>Check out my <a href="http://blog.donnaalmendrala.name">blog!</a> and <a href="http://portfolio.donnaalmendrala.name">portfolio!</a></p>
-    </div>
-    <div id="content">
-	<h2>Really! You should click it!</h2>
-	</div>
+<?php include('includes/header.php'); ?>
 
-</body>
-</html>
+		<div id="spot">	
+			<img src="images/mainspot.png" alt="Welcome to Donna Almendrala's Site" height="626px" width="865px" />    
+	    </div>
+
+<?php include('includes/footer.php'); ?>
---
+++ store.php
@@ -0,0 +1,48 @@
+<?php include('includes/header.php'); ?>
+
+		<div class="content">	
+			<img class="thumb" src="images/avastthumb.png" alt="AVAST!" height="220px" width="150px" />
+			
+			<p class="description">32-page, full color, high seas adventure comic based on Golden Age comics in 1941</p>
+			
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post"> <input name="cmd" type="hidden" value="_s-xclick" />
+			<table>
+			<tbody>
+			<tr>
+			<td><input name="on0" type="hidden" value="Shipping" />Shipping</td>
+			</tr>
+			<tr>
+			<td><select name="os0"> <option value="Domestic">Domestic $6.99</option> <option value="None">None $5.00</option> </select></td>
+			</tr>
+			</tbody>
+			</table>
+			<input name="currency_code" type="hidden" value="USD" /> <input name="encrypted" type="hidden" value="-----BEGIN PKCS7-----MIIHuQYJKoZIhvcNAQcEoIIHqjCCB6YCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAujo3ataZrpuJ4AGyICWaBf1DWdQacPLoRW0nwIlE1wHNMx0UkwzMGeAOh6dbU68+8JiD7ren2PFQ9pd/0IgV/sqGkoG+0M1qdZAFAG6Axj3WLRUfCvdYom/Yba5ovGsmJzWT7nQvnqbN3VA6pUH59hxASXPbQD1jY40xrYCJOJDELMAkGBSsOAwIaBQAwggE1BgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECI+3/jvjsERAgIIBEJ+UANNFNhUP5M3G/Zb6NrDHg2z5k7us7pOQOPliTtlOgX9SrcwMFPLKfeM6rjLP6S7zja+hawI32H9xR9g6r4TCoBJpnLqBxr85x5vzhU+RjQbqGaeCMOFrgBQCnghNK19lOsby2jSZxXg9kzDajunoE0JqsBqJkb13g7+eJaYiAnh4zVBzNvWOFifQ3nM5dig0dNPsgVFDWiwo54/odHRY3xrwDoLhki/zubpS9K3tm6zf0bnXV6VDlCZ6ALgOJw3+hLIOSPY3gj+DYK1BsC9yIQS6UpSP+QcX85Fz/VtPk1aT5S8HT8gXfOO+/nZ8q90VSBxihgdX4OEt7XOnmf8MUWizCALZRrnLNlg6yr2moIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTEwNDI4MjExNTQ1WjAjBgkqhkiG9w0BCQQxFgQUfabevEOZK8TU8im0teOUUue1sJswDQYJKoZIhvcNAQEBBQAEgYAtVf/XO5JRwLJQNqQFCgeoCrwexb+5CPGSrmYoCudOT+wp/kqWGbVtfWXDrXfmb3caM3zPwBkn6pBDT9p9E3seuY/2hMfY8yPVot25dUPC18OHACv0+HCSTTBH08epE5lHEjVPgY/bsfBi8eoWNoyPmCus6Gt3Tnh+VtFqw5+73w==-----END PKCS7-----" /> <input alt="PayPal - The safer, easier way to pay online!" name="submit" src="https://www.paypalobjects.com/WEBSCR-640-20110401-1/en_US/i/btn/btn_cart_LG.gif" type="image" /> <img src="https://www.paypalobjects.com/WEBSCR-640-20110401-1/en_US/i/scr/pixel.gif" border="0" alt="" width="1" height="1" />
+			
+			</form>
+			
+			<div class="clear"></div>
+			
+			<img class="thumb" src="images/mh_coverscan.png" alt="Monster Handler" height="171px" width="150px" />
+			
+			<p class="description">32 page, color covers, limited edition screen printed vellum jacket</p>
+
+			
+			<form action="https://www.paypal.com/cgi-bin/webscr" method="post"> <input name="cmd" type="hidden" value="_s-xclick" />
+			<table>
+			<tbody>
+			<tr>
+			<td><input name="on0" type="hidden" value="Shipping" />Shipping</td>
+			</tr>
+			<tr>
+			<td><select name="os0"> <option value="Domestic">Domestic $5.99</option> <option value="None">None $4.00</option> </select></td>
+			</tr>
+			</tbody>
+			</table>
+			<input name="currency_code" type="hidden" value="USD" /> <input name="encrypted" type="hidden" value="-----BEGIN PKCS7-----MIIH2QYJKoZIhvcNAQcEoIIHyjCCB8YCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYAUyX07ThyJYIYF11LzOm73Sc+PZQbLX6RB79cYSLis0mlirWGbH8dL5I6FJZQyiKdHjvewQ6EXNQf9k4vRSkeflDdnLXT6FTdi1RB45r1oivv3nm9FOTAmGGdyJY+NnZLaigGiCyWTtc7xPbyg9zCbL1WlLqz8Lox8xlJtow5XQjELMAkGBSsOAwIaBQAwggFVBgkqhkiG9w0BBwEwFAYIKoZIhvcNAwcECNMW9u0Tc4OlgIIBMD8kkqGTQlPRFidszwXbOFZyk50YySt3XMfJ4yyGYGCcOOyhocJDKZeHLU0DsCsrqdDBvTOvarAkVPkRnYTCwYjrPzKIKckfWJxcicLY7O8LI2k9ciDDRcouN7WOOrkfB6c+8/BQMvlOK7B8rxIk/utiREuuanmi1M7/DSwskDqEcxm48t6n6NHqvi+PpRTmhRRCoatFxglzn6Lze4iLhapOUnEI+JoquLsC6OLE1FL00zEOR2OyqQX0hJ6TftWAzRF1jOqfIymG2Mcy1jcLO7G6SmeeB2l+0pwi6J/JIm6A3lIAOFy1SEXC3/8oIBA2kP0eYekVk+vL8tqA6hSk0JsFPdVT2gJIyv4pe26L+E/fQiGPiKvrG6/um1YpFrhEwznNbsMhfBFDhek3sQIOBg+gggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xMTA0MjgyMTExMTNaMCMGCSqGSIb3DQEJBDEWBBTX3tehnWKHfbNigBHRKogIKnut2jANBgkqhkiG9w0BAQEFAASBgLAzDWqCBoYDQw6LlAshhrAigzAX3ab/oMAM9aVlIq4DYCyFv1uHzYkAU5vY4d2fkDaB7zU42H+owT1sEIdHb9Qsco8G0OpJWB4LEFZvvU6k9+IQhqIrIE6CCfHw1Sutf+pkVjmpurHUM93Tvn7/CmviyuCaHpwu6JBvw/OTb8JH-----END PKCS7-----" /> <input alt="PayPal - The safer, easier way to pay online!" name="submit" src="https://www.paypalobjects.com/WEBSCR-640-20110401-1/en_US/i/btn/btn_cart_LG.gif" type="image" /> <img src="https://www.paypalobjects.com/WEBSCR-640-20110401-1/en_US/i/scr/pixel.gif" border="0" alt="" width="1" height="1" />
+			
+			</form>
+			<div class="clear"></div>
+			
+	    </div>
+
+<?php include('includes/footer.php'); ?>
