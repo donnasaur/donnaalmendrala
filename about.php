b3fd16194b1c4a373f7c5047784e0efb3750e00d
Donna Almendrala <donna.almendrala@gmail.com>
2011-05-24 23:28:26 -0700

    changed image to a background url

--- about.php
+++ about.php
@@ -1,7 +1,7 @@
 <?php include('includes/header.php'); ?>
 
 		<div class="content">	
-			<img id="aboutPic" src="images/dino_pizza.png" alt="Mm, pizza…" width="300px" height="252px" />
+			<div id="aboutPic"> </div>
 			<p class="aboutText">Until further notice, Donna Almendrala is pursuing her love of comics as a Master of Fine Arts candidate (2012) at the Center for Cartoon Studies. In addition to cartooning, she studies illustration, design, and web and print production. She comes from a science background working as a research associate with a Bachelor of Science in Chemical Biology from the University of California, Berkeley (2008). She loves drawing, reading, and writing cartoons, and she dreams about it when she sleeps.</p>
 			<p class="aboutText">She likes computers and spends a lot of her free time learning programming languages so she can make comics about them. She is also an avid player of boardgames.</p>
 			<p class="aboutText">To contact her, please email <a href="mailto:captain@donnaalmendrala.name" target="_blank">captain@donnaalmendrala.name</a></p>
