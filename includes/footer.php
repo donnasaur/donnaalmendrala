4d6e2225b300f458279c94ab78c6732a23247257
Donna Almendrala <donna.almendrala@gmail.com>
2011-11-06 22:28:07 -0500

    redesigning site

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,3 @@
-		<div id="navigation">
-			<ul>
-				<li class="main"><a target="_self" href="index.php">Main</a></li>
-				<li class="store"><a target="_self" href="store.php">Store</a></li>
-				<li class="about"><a target="_self" href="about.php">About</a></li>
-				<li class="blog"><a target="_blank" href="http://blog.donnaalmendrala.name">Blog</a></li>
-				<li class="portfolio"><a target="_blank" href="http://portfolio.donnaalmendrala.name">Portfolio</a></li>
-			</ul>
-		</div>
 		<div id="copyright">
 		</div>
 	</div>
--- includes/header.php
+++ includes/header.php
@@ -12,10 +12,23 @@ Copyright 2011 Donna Almendrala (see AUTHORS)
 -->
 	<link rel="icon" type="image/png" href="images/favicon.png">
 	<title>Donna Almendrala</title>
-	<meta http-equiv="content-type" 
-		content="text/html;charset=utf-8" />
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<link href="stylesheets/styles.css" media="all" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
 	<div id="container">
+		<div class="head_banner">
+			<div id="balloon">
+			</div>
+			<div id="hatch_spot">
+			</div>
+		</div>
+		<div id="navigation">
+			<ul>
+				<li class="main"><a target="_self" href="index.php">Main</a></li>
+				<li class="about"><a target="_self" href="about.php">About</a></li>
+				<li class="store"><a target="_self" href="store.php">Store</a></li>
+				<li class="blog"><a target="_blank" href="http://blog.donnaalmendrala.name">Blog</a></li>
+			</ul>
+		</div>
