9bba8f8f69ce34f5b1355aa1c5b7bff1e4ff70d0
Donna Almendrala <donna.almendrala@gmail.com>
2011-11-13 17:57:43 -0500

    linked all thumbnails to images on gallery

--- index.php
+++ index.php
@@ -19,49 +19,49 @@ Copyright 2011 Donna Almendrala (see AUTHORS)
 				<div class="subcontents">
 					<h2>Baby</h2>
 						<div class="thumb baby_p1"><a href="images/baby_p1.png" rel="lightbox[Baby]" title="Baby page1">Baby p1</a></div>
-						<div class="thumb" id="baby_p2"><a href="images/baby_p2.png" rel="lightbox[Baby]" title="Baby page2">Baby p2</a></div>
-						<div class="thumb" id="baby_p3"><a href="images/baby_p3.png" rel="lightbox[Baby]" title="Baby page3">Baby p3</a></div>
-						<div class="thumb" id="baby_p4"><a href="images/baby_p4.png" rel="lightbox[Baby]" title="Baby page4">Baby p4</a></div>
+						<div class="thumb baby_p2"><a href="images/baby_p2.png" rel="lightbox[Baby]" title="Baby page2">Baby p2</a></div>
+						<div class="thumb baby_p3"><a href="images/baby_p3.png" rel="lightbox[Baby]" title="Baby page3">Baby p3</a></div>
+						<div class="thumb baby_p4"><a href="images/baby_p4.png" rel="lightbox[Baby]" title="Baby page4">Baby p4</a></div>
 					<div class="clear"></div>
 					<h2>Shower Secrets</h2>
-						<div class="thumb" id="showersecrets"><a href="images/showersecrets.png" rel="lightbox[Shower Secrets]" title="Shower Secrets from Lies My Parents Told Me anthology, 2011">Shower Secrets</a></div>
+						<div class="thumb showersecrets"><a href="images/showersecrets.png" rel="lightbox[Shower Secrets]" title="Shower Secrets from Lies My Parents Told Me anthology, 2011">Shower Secrets</a></div>
 					<div class="clear"></div>
 					<h2>The Concretes</h2>
-					<div class="thumb" id="concretes"><a href="images/concretes.png" rel="lightbox[NoisePop2011]" title="NoisePop 2011 Concert Review: http://2011.noisepop.com/comics/">NoisePop2011</a></div>
+					<div class="thumb concretes"><a href="images/concretes.png" rel="lightbox[NoisePop2011]" title="NoisePop 2011 Concert Review: http://2011.noisepop.com/comics/">NoisePop2011</a></div>
 					<div class="clear"></div>
 					<h2>Solstice Submarine</h2>
-					<div class="thumb" id="submarine_p1"><a href="images/submarine_p1.png" rel="lightbox[solsticesub]" title="Solstice Submarine page1">Solstice Submarine p1</a></div>
-					<div class="thumb" id="submarine_p2"><a href="images/submarine_p2.png" rel="lightbox[solsticesub]" title="Solstice Submarine page2">Solstice Submarine p2</a></div>
-					<div class="thumb" id="submarine_p3"><a href="images/submarine_p3.png" rel="lightbox[solsticesub]" title="Solstice Submarine page3">Solstice Submarine p3</a></div>
-					<div class="thumb" id="submarine_p4"><a href="images/submarine_p4.png" rel="lightbox[solsticesub]" title="Solstice Submarine page4">Solstice Submarine p4</a></div>
-					<div class="thumb" id="submarine_p5"><a href="images/submarine_p5.png" rel="lightbox[solsticesub]" title="Solstice Submarine page5">Solstice Submarine p5</a></div>
-					<div class="thumb" id="submarine_p6"><a href="images/submarine_p6.png" rel="lightbox[solsticesub]" title="Solstice Submarine page6">Solstice Submarine p6</a></div>
+					<div class="thumb submarine_p1"><a href="images/submarine_p1.png" rel="lightbox[solsticesub]" title="Solstice Submarine page1">Solstice Submarine p1</a></div>
+					<div class="thumb submarine_p2"><a href="images/submarine_p2.png" rel="lightbox[solsticesub]" title="Solstice Submarine page2">Solstice Submarine p2</a></div>
+					<div class="thumb submarine_p3"><a href="images/submarine_p3.png" rel="lightbox[solsticesub]" title="Solstice Submarine page3">Solstice Submarine p3</a></div>
+					<div class="thumb submarine_p4"><a href="images/submarine_p4.png" rel="lightbox[solsticesub]" title="Solstice Submarine page4">Solstice Submarine p4</a></div>
+					<div class="thumb submarine_p5"><a href="images/submarine_p5.png" rel="lightbox[solsticesub]" title="Solstice Submarine page5">Solstice Submarine p5</a></div>
+					<div class="thumb submarine_p6"><a href="images/submarine_p6.png" rel="lightbox[solsticesub]" title="Solstice Submarine page6">Solstice Submarine p6</a></div>
 					<div class="clear"></div>
 				</div>
 			<h1>Illustration</h1>
 				<div class="subcontents">
-					<div class="thumb" id="troll_gag"><a href="images/troll_gag.png" rel="lightbox[illustration]" title="Old Norse Gag">Troll Gag</a></div>
-					<div class="thumb" id="damonbot"><a href="images/damonbot.png" rel="lightbox[illustration]" title="Damon-Bot Avatar">Damon-Bot Avatar</a></div>
-					<div class="thumb" id="duckman"><a href="images/duckman.png" rel="lightbox[illustration]" title="Duckman Avatar">Duckman Avatar</a></div>
-					<div class="thumb" id="gitcard"><a href="images/gitcard.png" rel="lightbox[illustration]" title="Git Greeting Card">Git Card</a></div>
-					<div class="thumb" id="monsterhandler_cover"><a href="images/monsterhandler_cover.png" rel="lightbox[illustration]" title="Monster Handler Front Cover">Monster Handler</a></div>
+					<div class="thumb troll_gag"><a href="images/troll_gag.png" rel="lightbox[illustration]" title="Old Norse Gag">Troll Gag</a></div>
+					<div class="thumb damonbot"><a href="images/damonbot.png" rel="lightbox[illustration]" title="Damon-Bot Avatar">Damon-Bot Avatar</a></div>
+					<div class="thumb duckman"><a href="images/duckman.png" rel="lightbox[illustration]" title="Duckman Avatar">Duckman Avatar</a></div>
+					<div class="thumb gitcard"><a href="images/gitcard.png" rel="lightbox[illustration]" title="Git Greeting Card">Git Card</a></div>
+					<div class="thumb monsterhandler_cover"><a href="images/monsterhandler_cover.png" rel="lightbox[illustration]" title="Monster Handler Front Cover">Monster Handler</a></div>
 					<div class="clear"></div>
-					<div class="thumb" id="strangeromance"><a href="images/strangeromance.png" rel="lightbox[illustration]" title="Faux 1970's Romance Cover">Strange Romance</a></div>
-					<div class="thumb" id="mountaingoats"><a href="images/mountaingoats.png" rel="lightbox[illustration]" title="Faux Concert Flyer">Mountain Goats Concert</a></div>
-					<div class="thumb" id="screenprint_bio"><a href="images/screenprint_bio.png" rel="lightbox[illustration]" title="Screenprinted Self-Portrait">Self-Portrait</a></div>
-					<div class="thumb" id="bearjordan"><a href="images/bearjordan.png" rel="lightbox[illustration]" title="Bear Jordan Desktop Wallpaper">Bear Jordan</a></div>
+					<div class="thumb strangeromance"><a href="images/strangeromance.png" rel="lightbox[illustration]" title="Faux 1970's Romance Cover">Strange Romance</a></div>
+					<div class="thumb mountaingoats"><a href="images/mountaingoats.png" rel="lightbox[illustration]" title="Faux Concert Flyer">Mountain Goats Concert</a></div>
+					<div class="thumb screenprint_bio"><a href="images/screenprint_bio.png" rel="lightbox[illustration]" title="Screenprinted Self-Portrait">Self-Portrait</a></div>
+					<div class="thumb bearjordan"><a href="images/bearjordan.png" rel="lightbox[illustration]" title="Bear Jordan Desktop Wallpaper">Bear Jordan</a></div>
 					<div class="clear"></div>
 				</div>
 			<h1>Design</h1>
 				<div class="subcontents">
 					<h2>Piratehook</h2>
-					<div class="thumb" id="piratehook_1"><a href="images/piratehook_1.png" rel="lightbox[piratehook]" title="Static Image for Design of Piratehook.com">Under Construction</a></div>
-					<div class="thumb" id="piratehook_2"><a href="images/piratehook_2.png" rel="lightbox[piratehook]" title="Static Image for Design of Piratehook.com/404">404</a></div>
+					<div class="thumb piratehook_1"><a href="images/piratehook_1.png" rel="lightbox[piratehook]" title="Static Image for Design of Piratehook.com">Under Construction</a></div>
+					<div class="thumb piratehook_2"><a href="images/piratehook_2.png" rel="lightbox[piratehook]" title="Static Image for Design of Piratehook.com/404">404</a></div>
 					<div class="clear"></div>
 					<h2>Python Comics</h2>
-					<div class="thumb" id="python1"><a href="images/python1.png" rel="lightbox[pythoncomics]" title="Static Image 1 for Design of Pythoncomics.com">Python Comics 1</a></div>
-					<div class="thumb" id="python2"><a href="images/python2.png" rel="lightbox[pythoncomics]" title="Static Image 2 for Design of Pythoncomics.com">Python Comics 2</a></div>
-					<div class="thumb" id="python3"><a href="images/python3.png" rel="lightbox[pythoncomics]" title="Static Image 3 for Design of Pythoncomics.com">Python Comics 3</a></div>
+					<div class="thumb python1"><a href="images/python1.png" rel="lightbox[pythoncomics]" title="Static Image 1 for Design of Pythoncomics.com">Python Comics 1</a></div>
+					<div class="thumb python2"><a href="images/python2.png" rel="lightbox[pythoncomics]" title="Static Image 2 for Design of Pythoncomics.com">Python Comics 2</a></div>
+					<div class="thumb python3"><a href="images/python3.png" rel="lightbox[pythoncomics]" title="Static Image 3 for Design of Pythoncomics.com">Python Comics 3</a></div>
 					<div class="clear"></div>
 				</div>
 		</div>
