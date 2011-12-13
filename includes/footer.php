42cc1f2a6f3297b9001a72e59ef310f1dfa7668b
Donna Almendrala <donna.almendrala@gmail.com>
2011-12-13 16:51:48 -0500

    changed link to jquery

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 		<div id="copyright">Copyright &copy; Donna Almendrala, 2012
 		</div>
 	</div>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+	<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
 	<script type="text/javascript" src="js/donnaalmendrala.js"></script>
 	<script type="text/javascript" src="js/jquery.fancybox-1.3.4.pack.js"></script>
 
