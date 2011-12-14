7a8b6bcb05e0c0d81933f42291d59724fc7d6708
Donna Almendrala <donna.almendrala@gmail.com>
2011-12-14 00:29:45 -0500

    added paypalbutton

--- store.php
+++ store.php
@@ -9,19 +9,46 @@
 			
 			<p class="description">32-page, full color, high seas adventure comic based on Golden Age comics in 1941</p>
 			
-			<p class="description">Price: $5.00</p>
+			<p class="description">Price: $5.00 + 1.99 shipping</p>
 			
-			<div class="product"><input type="hidden" class="product-title" value="AVAST!"><input type="hidden" class="product-price" value="5.00"><div class="googlecart-add-button" tabindex="0" role="button" title="Add to cart"></div></div>			
+			<p class="description">Buy with Google Checkout</p>
+			
+			<div class="product"><input type="hidden" class="product-title" value="AVAST!"><input type="hidden" class="product-price" value="5.00"><div class="googlecart-add-button" tabindex="0" role="button" title="Add to cart"></div></div>
+			
+			<p class="description">Buy with PayPal</p>
+			
+			<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHVwYJKoZIhvcNAQcEoIIHSDCCB0QCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYCyzg+BRoRZwvc5Bn5mqca/bUvZ9bFqTUgIGdodYiOPjjm8NMUAhtw4ytlziZvXMnmWqc5H8fPj6tHETbUopYsfrqhlS8CVWux6UKXq95MEBewgwXn/T6Afl2VI+c4QlItWSHtuEqpRzPUypPYyrskLpOB+2jM0H898JqV2FVXF/DELMAkGBSsOAwIaBQAwgdQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQINLHt4N/ZuXaAgbDl0DRsU7nvKC69KOfMPvjXZ6cQlJKnEw6Q8UDyRaYypXW6reeX6N5VGpSLcDoXFJTxVGq8BVDEQncFYInB/eiVRBsZWIMkt7aEmYbTbTA2tEl3yEfrKa1n2se04T1w8/EoOZcJrwZl88NRpz6XekA5yEN5LMmUpW4BDOZ2WivLpD980YJ4w3gdhnHQcKqACLdra8Auj+PlBpUqLfshk0iaL0mEwFeFqKIxeMwIyOhAFqCCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTExMTIxNDA1MTEzNFowIwYJKoZIhvcNAQkEMRYEFNxmt+Zz9hlZELGxwQzlQeiB4aEnMA0GCSqGSIb3DQEBAQUABIGAsoJmKrbfgXV5M2k3KsE95B+75oGayvK25xKYLF1yqVeRifcQd7ocprVAqTJ2nOoD6T0IC3jmUdwS7LHcPt9Li4QDWURjexiW49Rd0819SQEsZsaiUHPBu5r0hUoueL5zkIFdWGMch7mboxjWB4FMEL1DNEE/CyVbIoKffj+W7yA=-----END PKCS7-----
+">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+			
 			<div class="clear"></div>
 			
 			<div id="mhthumb"></div>
 			
-			<p class="description">32 page, color covers, limited edition screen printed vellum jacket</p>
-
-			<p class="description">Price: $4.00</p>
+			<p class="description">32-page, color covers, limited edition screen printed vellum jacket</p>
+			<p class="description">Price: $4.00 + 1.99 shipping</p>
+			
+			<p class="description">Buy with Google Checkout</p>
 			
 			<div class="product"><input type="hidden" class="product-title" value="Monster Handler"><input type="hidden" class="product-price" value="4.00"><div class="googlecart-add-button" tabindex="0" role="button" title="Add to cart"></div></div>		
+			
+			<p class="description">Buy with PayPal</p>
+			
+			<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHXwYJKoZIhvcNAQcEoIIHUDCCB0wCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBPLyAJBnaQLuucKq2BbqfNc7BLQog62ngv3cVo8JN+P/loN4B5Hp2RPheeE6jt0AMfwQz9LMxyycLB/8YMG7PLu1HyUrUX6oyAHHno1MQG8/0c6ST96/D3HayJ/lYy+dpp+auEw2aksCocrvKNnD6xrgbt/GhL1EsVJ4DnujKYPTELMAkGBSsOAwIaBQAwgdwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQI6lvYgA4aS66AgbgfzQX7orksJONt8euu/V1VoJ9J0mzFCvhU0b74qFp+SWrkyRRz1EHQzDJNx07BBVk/c3zss9X9yxK9BBFvtX+MOEEUbz8CzzayP8QjDOcge4wDH2zk7e5jPNg9doC2iejEvjfAAsJM209MAtTBQhfcqkM3suelpCGjyF3mAwx0Jr9nx0nuUktwUVqwbS89CjdMHlo5WmSgnMZT+CSAbLuUtQwOLouxrlUDJ73o8NBDiVuKcuapG+agoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTExMjE0MDUxNjE0WjAjBgkqhkiG9w0BCQQxFgQU6DwFleK2eg//wfCPdP1xLgZuKLowDQYJKoZIhvcNAQEBBQAEgYABx3X/Hw4BljLxkliLTC5AkQ3GkluV+LbIsJgT2xY4ZpoXBg7nAR8HjwQ3OzsRw9uNjke8AcFgeMYbYkX5gJ89QxMgglRzYM4D/hSCicpZEU6Y6cUDGoAZNCW5gwoMNLnI9jbZ1LESfGlhbsZBSiO4OkGMCCchiqobPE9BBeO/bA==-----END PKCS7-----
+">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+		
 			<div class="clear"></div>
+		
 			
 	    </div>
 	    
