b39d8e3fbc350eb6d02ac627fd458816974aa181
Donna Almendrala <donna.almendrala@gmail.com>
2012-01-17 22:08:17 -0500

    made the chimp sprite part of the anchor tag

--- chimpsinspace/index.php
+++ chimpsinspace/index.php
@@ -11,15 +11,17 @@ the GNU GPL v3 licenses (see GPL-LICENSE).
 Copyright 2011 Donna Almendrala (see AUTHORS)
 -->
 	<link rel="icon" type="image/png" href="images/favicon.png">
-	<title>Chimps in Space Story Generator</title>
+	<title>Chimps in Space: Random Story Generator</title>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<meta name="description" content="Chimps in Space Story Generator" />
 	<link href="stylesheets/styles.css" media="all" rel="stylesheet" type="text/css" />
 </head>
 <body>
-	<div id="container">
-		<div id="sprite"><img src="images/sprite_1.gif"></div>	
-		<div id="title"><a href="javascript:void()" class="generate"><img src="images/cis_title.png" height="38px" width="511px" alt="Chimps in Space" /></a></div>
+	<div id="starfield">
+		<div class="star"></div>
+	</div>
+	<div id="container">	
+		<div id="title"><a href="javascript:void()" class="generate"><div id="sprite"><img src="images/sprite_1.gif"></div><img src="images/cis_title.png" height="38px" width="511px" alt="Chimps in Space" /></a></div>
     	<div class="genre">genre</div>
     	<div class="setting">setting</div>
     	<div class="plot">plot</div>
