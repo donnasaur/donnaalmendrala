acf7706df1e526ae9b6473405312052f1b38665d
Donna Almendrala <donna.almendrala@gmail.com>
2012-06-16 18:50:07 -0400

    edited about entry

--- about.php
+++ about.php
@@ -5,9 +5,9 @@
 <?php include('includes/navigation.php'); ?>
 
 		<div class="content">
-			<p class="aboutText">Until further notice, Donna Almendrala is pursuing her love of comics at the Center for Cartoon Studies where received her Master of Fine Arts degree in Spring 2012. In addition to cartooning, she studies illustration, design, and web and print production. She comes from a science background working as a research associate with a Bachelor of Science in Chemical Biology from the University of California, Berkeley (2008). She loves drawing, reading, and writing cartoons, and she dreams about it when she sleeps.</p>
-			<p class="aboutText">She likes computers and spends a lot of her free time learning programming languages so she can make comics about them. She is also an avid player of boardgames.</p>
-			<p class="aboutText">For questions, comments, or hiring inquiries please send email to <a href="mailto:captain@donnaalmendrala.name" target="_blank">captain@donnaalmendrala.name</a></p>
+			<p>Until further notice, Donna Almendrala is pursuing her love of comics at the Center for Cartoon Studies where she received her Master of Fine Arts degree in Spring 2012. In addition to cartooning, she studied illustration, design, and web and print production. She comes from a science background working as a research associate with a Bachelor of Science in Chemical Biology from the University of California, Berkeley (2008). She loves drawing, reading, and writing cartoons, and she dreams about it when she sleeps.</p>
+			<p>She likes computers and spends a lot of her free time learning programming languages so she can make comics about them. She is also an avid player of boardgames.</p>
+			<p>For questions, comments, or hiring inquiries please send email to <a href="mailto:captain@donnaalmendrala.name" target="_blank">captain@donnaalmendrala.name</a></p>
 			
 		</div>
 	    </div>
