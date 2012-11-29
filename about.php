2fab2a6c7de84b7561e52c258a2a94c03f19cad7
Donna Almendrala <donna.almendrala@gmail.com>
2012-11-29 15:16:32 -0800

    changing link to chimps-in-space.com

--- about.php
+++ about.php
@@ -8,7 +8,7 @@
 <?php include('includes/navigation.php'); ?>
 
         <div class="content">
-            <p>Donna Almendrala is currently working on a series called <a href="store.php">Chimps in Space</a> which is posted each week as a <a href="http://chimpsinspace.donnaalmendrala.name">webcomic.</a></p>
+            <p>Donna Almendrala is currently working on a series called <a href="store.php">Chimps in Space</a> which is posted each week as a <a href="http://chimps-in-space.com">webcomic.</a></p>
             <p> She received her Master of Fine Arts degree from the Center for Cartoon Studies in Spring 2012 and now resides in the Bay Area, California. In addition to cartooning, she studied illustration, design, and web and print production. She comes from a science background working as a research associate with a Bachelor of Science in Chemical Biology from the University of California, Berkeley (2008). She loves drawing, reading, and writing cartoons, and she dreams about it when she sleeps.</p>
 			<p>She likes computers and spends a lot of her free time learning programming languages so she can make comics about them. She is also an avid player of boardgames.</p>
 			<p>For questions, comments, or hiring inquiries please send email to <a href="mailto:captain@donnaalmendrala.name" target="_blank">captain@donnaalmendrala.name</a></p>
