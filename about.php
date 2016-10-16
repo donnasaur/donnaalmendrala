2d3e3454739f6910b0418808d38df2fed2883096
Donna <me@donnaalmendrala.name>
2016-10-16 16:33:58 -0700

    Updated about page

--- about.php
+++ about.php
@@ -2,17 +2,24 @@
 		<title>Cartoonist | Designer | About | Donna Almendrala</title>
 <?php include('includes/header_b.php'); ?>
 
-		
+
 		<div id="pizza_spot">
-			
+
 <?php include('includes/navigation.php'); ?>
 
         <div class="content">
-            <p>Donna Almendrala is currently working on a series called <a href="store.php">Chimps in Space</a> which is posted each week as a <a href="http://chimps-in-space.com">webcomic.</a></p>
-            <p> She received her Master of Fine Arts degree from the Center for Cartoon Studies in Spring 2012 and now resides in the Bay Area, California. In addition to cartooning, she studied illustration, design, and web and print production. She comes from a science background working as a research associate with a Bachelor of Science in Chemical Biology from the University of California, Berkeley (2008). She loves drawing, reading, and writing cartoons, and she dreams about it when she sleeps.</p>
-			<p>She likes computers and spends a lot of her free time learning programming languages so she can make comics about them. She is also an avid player of boardgames.</p>
-			<p>For questions, comments, or hiring inquiries please send email to <a href="mailto:captain@donnaalmendrala.name" target="_blank">captain@donnaalmendrala.name</a></p>
-			
+
+            <p>Donna received her Master of Fine Arts degree from the Center for Cartoon Studies in Spring 2012 and now resides in the Bay Area, California. In addition to cartooning, she studied illustration, design, and web and print production. She comes from a science background working as a research associate with a Bachelor of Science in Chemical Biology from the University of California, Berkeley (2008). She loves drawing, reading, and writing cartoons, and she dreams about it when she sleeps.</p>
+						<p>She likes tech and spends a lot of her free time learning programming languages so she can make comics about them. She is also an avid player of boardgames.</p>
+						<p>Check out her other projects:</p>
+						<ul>
+							<li><a href="http://wanderersguide.tothewilds.online/" target="_blank">The Wanderer's Guide to the Wilds</a></li>
+							<li><a href="http://rollpluswis.tumblr.com/" target="_blank">Roll Plus Wis</a></li>
+							<li><a href="http://chimps-in-space.com" target="_blank">Chimps in Space</a>
+						</ul>
+						<p>Follow her on <a href="https://twitter.com/madmacaques" target="_blank">twitter</a> or <a href="http://madmacaques.tumblr.com/" target="_blank">tumblr</a>.</p>
+						<p>For questions, comments, or hiring inquiries please send email to <a href="mailto:hello@donnaalmendrala.name" target="_blank">hello@donnaalmendrala.name</a></p>
+
 		</div>
 	    </div>
 
